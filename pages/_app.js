ba6f421feff21c9709523ebecbd0ebe0537efb6f
Dhairya Shah <dtshah24022006@gmail.com>
2022-07-15 23:16:02 +0530

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-
+import NextNProgress from 'nextjs-progressbar'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress color="linear-gradient(to right, rgb(134, 239, 172), rgb(59, 130, 246), rgb(147, 51, 234))" />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/hello.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Hello() {
+  return (
+    <>
+        <div className="container">
+            <h1>Hello world</h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt proident labore veniam fugiat occaecat. Ipsum esse voluptate voluptate ad quis occaecat eu culpa enim id id. Qui excepteur amet qui deserunt aute mollit elit. Esse do commodo aute qui eiusmod culpa sint laborum. Officia aute ipsum reprehenderit pariatur minim velit et tempor. Minim adipisicing pariatur mollit cupidatat enim in cillum nulla tempor ipsum elit velit aute. Ipsum exercitation commodo consequat ex enim quis quis amet cillum. Do reprehenderit velit dolor reprehenderit.
+            Laborum aliquip cillum deserunt id sunt dolor ad exercitation consectetur in voluptate tempor. Voluptate tempor eiusmod excepteur reprehenderit et quis et officia tempor ad. Qui mollit duis ullamco nulla laborum laboris irure magna enim in proident irure occaecat labore sint. Minim qui mollit fugiat nisi velit laboris do ullamco cupidatat et proident et aliqua proident magna.
+            Nostrud incididunt labore et aliquip. Ipsum ad nulla tempor id aute ex minim elit fugiat aliqua ut non excepteur nostrud ullamco. Commodo pariatur commodo magna minim occaecat in dolore et proident. Qui ea esse consequat pariatur dolore occaecat deserunt id consectetur irure officia est eiusmod elit aute.
+            </p>
+            <Link href="/">
+          <a className="link" style={{textDecoration: "underline"}}>Go back home</a>
+        </Link>
+        </div>
+    </>
+  )
+}
